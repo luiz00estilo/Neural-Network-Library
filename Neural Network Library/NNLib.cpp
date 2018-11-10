2d6a758d08768f53b68fcba723a12830487d16d8
luiz00estilo <luizhomartins@hotmail.com>
2018-11-10 20:44:13 -0300

    Memory Allocation bugfix (on constructor)

--- Neural Network Library/NNLib.cpp
+++ Neural Network Library/NNLib.cpp
@@ -304,14 +304,14 @@ neuralNetwork::neuralNetwork(const int layerCount, const int inputLen, const int
 	layerLen[netLen - 1] = outputLen;								//Sets the length of the output layer
 	value = new long double*[netLen];											//Creates the value layers
 	for (int x = 0; x < netLen; x++) value[x] = new long double[layerLen[x]];	//Creates the values for each layer
-	//std::uniform_real_distribution<long double> rnd(-1, 1);													//}Creates the randomizer,
-	//std::default_random_engine eng(std::chrono::high_resolution_clock::now().time_since_epoch().count());	//}generating a number between (min, max)
+	std::uniform_real_distribution<long double> rnd(-1, 1);													//}Creates the randomizer,
+	std::default_random_engine eng(std::chrono::high_resolution_clock::now().time_since_epoch().count());	//}generating a number between (min, max)
 	weight = new long double**[netLen - 1];	//Creates the weight layers
 	for (int x = 0; x < (netLen - 1); x++) {
 		weight[x] = new long double*[layerLen[x]];	//Creates the hidden weight nodes
 		for (int y = 0; y < layerLen[x]; y++) {
 			weight[x][y] = new long double[layerLen[x + 1]];						//Creates the weights
-			//for (int z = 0; z < layerLen[x + 1]; z++) weight[x][y][z] = rnd(eng);	//Randomizes the weights' values
+			for (int z = 0; z < layerLen[x + 1]; z++) weight[x][y][z] = rnd(eng);	//Randomizes the weights' values
 		}
 	}
 }
@@ -320,22 +320,17 @@ neuralNetwork::neuralNetwork(const int layerCount, const int* layersLen)
 	if (layerCount < 2 || layersLen[0] < 1 || layersLen[layerCount - 1] < 1) throw invArgExpt();				//}Validating the arguments
 	if (layerCount > 2) for (int x = 1; x < (layerCount - 1); x++) if (layersLen[x] < 1) throw invArgExpt();	//}
 	netLen = layerCount;	//Sets the number of layers
-	layerLen = new int[layerCount];				//Creates the list of layer lengths
-	//layerLen[0] = inputLen;						//Sets the length of the input layer
-	value = new long double*[layerCount];		//Creates the value layers
-	//value[0] = new long double[inputLen];			//Creates the input layer values
-	weight = new long double**[layerCount - 1];	//Creates the weight layers
-	//weight[0] = new long double*[inputLen];		//Creates the input weight nodes
-	for (int x = 0; x < layerCount; x++) {
+	layerLen = new int[netLen];				//Creates the list of layer lengths
+	value = new long double*[netLen];		//Creates the value layers
+	weight = new long double**[netLen - 1];	//Creates the weight layers
+	for (int x = 0; x < netLen; x++) {
 		layerLen[x] = layersLen[x];					//Sets the length of the hidden layers
 		value[x] = new long double[layersLen[x]];	//Creates the hidden layers values
-		weight[x] = new long double*[layersLen[x]];	//Creates the hidden weight nodes
 	}
-	//layerLen[layerCount - 1] = outputLen;				//Sets the length of the output layer
-	//value[layerCount - 1] = new long double[outputLen];	//Creates the output layer values
 	std::uniform_real_distribution<long double> rnd(-1, 1);													//}Creates the randomizer
 	std::default_random_engine eng(std::chrono::high_resolution_clock::now().time_since_epoch().count());	//}Generating a number between (min, max)
 	for (int x = 0; x < (netLen - 1); x++) {
+		weight[x] = new long double*[layersLen[x]];	//Creates the hidden weight nodes
 		for (int y = 0; y < layerLen[x]; y++) {
 			weight[x][y] = new long double[layerLen[x + 1]];						//Creates the weights
 			for (int z = 0; z < layerLen[x + 1]; z++) weight[x][y][z] = rnd(eng);	//Randomizes the weights' values
